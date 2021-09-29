1bd64a43c9e52b559778e4b42ff7c74616927a09
gary <gary.white@lodgit.com>
2021-09-29 13:11:46 +0200

    code working

--- kataNeedle.js
+++ kataNeedle.js
@@ -1,7 +1,34 @@
+// Rough version:
+//
+// function findNeedle(haystack) {
+//     // your code here
+//     // return haystack // returns array
+//     // return haystack.length; // returns 10
+//     var ans = 0;
+//     for (var i = 0; i < haystack.length; i++){
+//         ans = ans + 1;
+//         if (haystack[i] == 'needle') {
+//             ans = ans -1;
+//             return 'found the needle at position ' + ans;
+//         }
+//     }
+//     console.log(ans);
+//   }
+
+// Refactored version:
+
 function findNeedle(haystack) {
-    // your code here
-  }
+    var ans = 0;
+    for (var i = 0; i < haystack.length; i++){
+        ans = ans + 1;
+        if (haystack[i] == 'needle') {
+            ans = ans -1;
+            return 'found the needle at position ' + ans;
+        }
+    }
+}
 
-  var haystack_1 = ['3', '123124234', undefined, 'needle', 'world', 'hay', 2, '3', true, false];
 
-  console.log(findNeedle(haystack_1)); // returns 'found the needle at position 3'
+
+
+  console.log(findNeedle(['3', '123124234', undefined, 'needle', 'world', 'hay', 2, '3', true, false])); // returns 'found the needle at position 3'
